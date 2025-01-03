7ee8884d9fc19037d4fd3f054f51025d1d070049
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-03 17:48:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let para = document.querySelector("#status");
+let btn = document.querySelector("#enterBtn");
+
+btn.addEventListener("click", () => {
+	para.innerHTML = "<i>Entered Metaverse<i>";
+})
